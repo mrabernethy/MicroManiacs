3b730a4d6257fa80779d34b20e3555931462256b
Michael Abernethy <michael.r.abernethy@gmail.com>
2014-10-05 15:49:35 +1300

    Messing about with input.
    About to migrate input to new class and set up mouse rotation.

--- src/mygame/ClientMain.java
+++ src/mygame/ClientMain.java
@@ -1,10 +1,15 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.ChaseCamera;
 import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseAxisTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
+import com.jme3.input.controls.Trigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
@@ -39,7 +44,31 @@ implements ClientStateListener
     private Client myClient;
     private Vector3f lastSentPosition;
     
+    // Chase camera
+    //private ChaseCamera chaseCamera;
+    
+    private Player player;
     private HashMap<Integer, Player> players = new HashMap();
+    
+    private final static Trigger TRIGGER_W = new KeyTrigger(KeyInput.KEY_W);
+    private final static Trigger TRIGGER_S = new KeyTrigger(KeyInput.KEY_S);
+    private final static Trigger TRIGGER_A = new KeyTrigger(KeyInput.KEY_A);
+    private final static Trigger TRIGGER_D = new KeyTrigger(KeyInput.KEY_D);
+    private final static Trigger TRIGGER_UP = new KeyTrigger(KeyInput.KEY_UP);
+    private final static Trigger TRIGGER_DOWN = new KeyTrigger(KeyInput.KEY_DOWN);
+    private final static Trigger TRIGGER_LEFT = new KeyTrigger(KeyInput.KEY_LEFT);
+    private final static Trigger TRIGGER_RIGHT = new KeyTrigger(KeyInput.KEY_RIGHT);
+    // TODO: add shoot 
+    //private final static Trigger TRIGGER_SPACE = new KeyTrigger(KeyInput.KEY_SPACE);
+    private final static Trigger TRIGGER_ROTATE_X = new MouseAxisTrigger(MouseInput.AXIS_X, true);
+    private final static Trigger TRIGGER_ROTATE_Y = new MouseAxisTrigger(MouseInput.AXIS_Y, true);
+    
+    private final static String  MAPPING_UP = "Up";
+    private final static String  MAPPING_DOWN = "Down";
+    private final static String  MAPPING_LEFT = "Left";
+    private final static String  MAPPING_RIGHT = "Right";
+    //private final static String  MAPPING_SHOOT = "Shoot";
+    private final static String  MAPPING_ROTATE = "Rotate";
     
     
     public static void main(String[] args) 
@@ -61,21 +90,28 @@ implements ClientStateListener
             
         }
         
-        // Init Mappings and Listeners
-        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A), new KeyTrigger(KeyInput.KEY_LEFT));
-        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D), new KeyTrigger(KeyInput.KEY_RIGHT));
-        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W), new KeyTrigger(KeyInput.KEY_UP));
-        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S), new KeyTrigger(KeyInput.KEY_DOWN));
+        // Add the chase camera
+        //chaseCamera = new ChaseCamera(Camera cam);
         
-        inputManager.addListener(actionListener, new String[]{"Left", "Right", "Up", "Down"});
-        inputManager.addListener(analogListener, "");
+        // Init Mappings and Listeners
+        inputManager.addMapping(MAPPING_UP, TRIGGER_W, TRIGGER_UP);
+        inputManager.addMapping(MAPPING_DOWN, TRIGGER_S, TRIGGER_DOWN);
+        inputManager.addMapping(MAPPING_LEFT, TRIGGER_A, TRIGGER_LEFT);
+        inputManager.addMapping(MAPPING_RIGHT, TRIGGER_D, TRIGGER_RIGHT);
+        //inputManager.addMapping(MAPPING_SHOOT, TRIGGER_SPACE);
+        inputManager.addMapping(MAPPING_ROTATE, TRIGGER_ROTATE_X, TRIGGER_ROTATE_Y);
+        // TODO: add space mapping to listener
+        inputManager.addListener(actionListener, new String[]{MAPPING_UP, MAPPING_DOWN, 
+            MAPPING_LEFT, MAPPING_RIGHT});
+        inputManager.addListener(analogListener, new String[]{MAPPING_ROTATE});
         
         spriteManager = new SpriteManager(1024, 1024, SpriteMesh.Strategy.ALLOCATE_NEW_BUFFER, rootNode, assetManager);
         getStateManager().attach(spriteManager);
         
+        // Register the message classes
         Serializer.registerClass(ClientMessage.class);
-        Serializer.registerClass(GreetingMessage.class); // register the message class
-
+        Serializer.registerClass(GreetingMessage.class); 
+        // Add the message listeners
         myClient.addMessageListener(new ClientListener(this, myClient),
                 GreetingMessage.class);
         myClient.addMessageListener(new ClientListener(this, myClient),
@@ -89,6 +125,8 @@ implements ClientStateListener
         lastSentPosition = new Vector3f(this.cam.getLocation());
         
         attachCube(); // attaches a cube to the spatial
+        
+        // TODO: attach the world
         //attachWorld();
         
         this.flyCam.setEnabled(false);
@@ -98,9 +136,10 @@ implements ClientStateListener
     }
     
     /* Add some demo content */
+    // Currently used for the background and set to white.
     public void attachCube() {
         
-        Box box = new Box(3,1,0);
+        Box box = new Box(3,3,0);
         Geometry geom = new Geometry("Cube", box);
         Material mat = new Material(assetManager,
                 "Common/MatDefs/Misc/Unshaded.j3md");
@@ -125,10 +164,11 @@ implements ClientStateListener
     {
         SpriteImage spriteImage = spriteManager.createSpriteImage("smile.jpg", false);
         
-        Player p = new Player(new Vector3f(0,0,0), spriteImage);
-        p.getSprite().setSize(0.4f);
-        
-        players.put(id, p);
+        player = new Player(new Vector3f(0,0,0), spriteImage);
+        player.getSprite().setSize(0.4f);
+        // TODO: add chase camera to this player
+        //player.addControl(chaseCamera);
+        players.put(id, player);
     }
     
     public boolean playerExists(int id)
@@ -180,6 +220,101 @@ implements ClientStateListener
         super.destroy();
     }
 
+    
+    // use for shoot and click events
+    private ActionListener actionListener = new ActionListener() {
+        public void onAction(String name, boolean keyPressed, float tpf) {
+        /** TODO: test for mapping names and implement actions */
+            System.out.println("Mapping detected (discrete): "+ name);
+            player = players.get(myClient.getId());                   // made player a class variable
+            
+            // LEFT
+            if(name.equals(MAPPING_LEFT) && keyPressed)
+            {
+                // Set player left velocity
+                player.setVelocity(player.getVelocity().setX(-2));
+                
+            }
+            else if(name.equals(MAPPING_LEFT) && !keyPressed)
+            {
+                // Stop player left velocity
+                player.setVelocity(player.getVelocity().setX(0));
+            }
+            
+            // RIGHT
+            if(name.equals(MAPPING_RIGHT) && keyPressed)
+            {
+                // Set player right velocity
+                player.setVelocity(player.getVelocity().setX(2));
+                
+            }
+            else if(name.equals(MAPPING_RIGHT) && !keyPressed)
+            {
+                // Stop player right velocity
+                player.setVelocity(player.getVelocity().setX(0));
+            }
+            
+            // UP
+            if(name.equals(MAPPING_UP) && keyPressed)
+            {
+                // Set player up velocity
+                player.setVelocity(player.getVelocity().setY(2));
+                
+            }
+            else if(name.equals(MAPPING_UP) && !keyPressed)
+            {
+                // Stop player up velocity
+                player.setVelocity(player.getVelocity().setY(0));
+            }
+            
+            // DOWN
+            if(name.equals(MAPPING_DOWN) && keyPressed)
+            {
+                // Set player down velocity
+                player.setVelocity(player.getVelocity().setY(-2));
+            }
+            else if(name.equals(MAPPING_DOWN) && !keyPressed)
+            {
+                // Stop player down velocity
+                player.setVelocity(player.getVelocity().setY(0));
+            }
+            
+        }
+    };
+    
+    private AnalogListener analogListener = new AnalogListener() {
+
+        public void onAnalog(String name, float intensity, float tpf) {
+            System.out.println("Mapping detected (analog): "+ name + " " + intensity );
+            
+            if (name.equals(MAPPING_ROTATE)) {
+                // TODO: add rotate functin to player, rotate the sprite
+                // player.rotate(0, intensity * 10f, 0);
+            } 
+            // Tried to get it to work with intensity but no good
+//            else if (name.equals(MAPPING_UP))
+//            {
+//                // Set player up velocity
+//                player.setVelocity(player.getVelocity().setY(1000f*intensity));
+//            }
+//            else if (name.equals(MAPPING_DOWN))
+//            {
+//                // Set player up velocity
+//                player.setVelocity(player.getVelocity().setY(-1000f*intensity));
+//            }
+//            else if (name.equals(MAPPING_LEFT))
+//            {
+//                // Set player up velocity
+//                player.setVelocity(player.getVelocity().setX(-1000f*intensity));
+//            }
+//            else if (name.equals(MAPPING_RIGHT))
+//            {
+//                // Set player up velocity
+//                player.setVelocity(player.getVelocity().setX(1000f*intensity));
+//            }
+        }
+    };
+    
     /** Specify what happens when this client connects to server */
     public void clientConnected(Client client) 
     {
@@ -198,71 +333,4 @@ implements ClientStateListener
     {
         System.out.println("Client #" + client.getId() + " has left.");
     }
-    
-    private ActionListener actionListener = new ActionListener() {
-        public void onAction(String name, boolean keyPressed, float tpf) {
-        /** TODO: test for mapping names and implement actions */
-            
-            Player player = players.get(myClient.getId());
-            
-            // LEFT
-            if(name.equals("Left") && keyPressed)
-            {
-                // Set player left velocity
-                player.setVelocity(player.getVelocity().setX(-0.5f));
-                
-            }
-            else if(name.equals("Left") && !keyPressed)
-            {
-                // Stop player left velocity
-                player.setVelocity(player.getVelocity().setX(0));
-            }
-            
-            // RIGHT
-            if(name.equals("Right") && keyPressed)
-            {
-                // Set player right velocity
-                player.setVelocity(player.getVelocity().setX(0.5f));
-                
-            }
-            else if(name.equals("Right") && !keyPressed)
-            {
-                // Stop player right velocity
-                player.setVelocity(player.getVelocity().setX(0));
-            }
-            
-            // UP
-            if(name.equals("Up") && keyPressed)
-            {
-                // Set player up velocity
-                player.setVelocity(player.getVelocity().setY(0.5f));
-                
-            }
-            else if(name.equals("Up") && !keyPressed)
-            {
-                // Stop player up velocity
-                player.setVelocity(player.getVelocity().setY(0));
-            }
-            
-            // DOWN
-            if(name.equals("Down") && keyPressed)
-            {
-                // Set player down velocity
-                player.setVelocity(player.getVelocity().setY(-0.5f));
-            }
-            else if(name.equals("Down") && !keyPressed)
-            {
-                // Stop player down velocity
-                player.setVelocity(player.getVelocity().setY(0));
-            }
-            
-        }
-    };
-    
-    private AnalogListener analogListener = new AnalogListener() {
-        public void onAnalog(String name, float keyPressed, float tpf) {
-        /** TODO: test for mapping names and implement actions */
-            
-        }
-    };
 }
